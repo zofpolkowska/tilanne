7adc5248dd435d7865338d7330e6bf0d754bccc6
zofpolkowska <zofpolkowska@gmail.com>
2018-05-24 14:23:16 +0200

    moving to umbrella project and introducing Phoenix

--- apps/tilanne_api/web/views/welcome_view.ex
+++ apps/tilanne_api/web/views/welcome_view.ex
@@ -2,10 +2,10 @@ defmodule TilanneApi.WelcomeView do
   use TilanneApi.Web, :view
 
   def render("index.json", _assigns) do
-    %{
+    [%{
       description: "API for Tilanne",
       authors: "Zofia Polkowska & Jan Gapski"
-    }
+    }]
   end
 
 end
